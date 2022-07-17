67487bf6bb814c123272d988c9de1260c497067f
unknown <ahmed.asoliman1@gmail.com>
2022-07-17 04:25:44 +0200

    connect with apiURL by axios

---
+++ client/src/services/taskServices.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const apiURL = "http://localhost:8080/api/tasks";
+
+export function getTasks(){
+    return axios.get(apiURL);
+}
+
+export function addTask(task){
+    return axios.post(apiURL, task);
+}
+
+export function updateTask(id, task){
+    return axios.put(apiURL + '/' + id , task);
+}
+
+export function deleteTask(id){
+    return axios.delete(apiURL + '/' + id);
+}
